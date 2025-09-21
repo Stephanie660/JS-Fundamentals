5df78119c3ac762fbeb59f95ecbbc8800c455e75
stephanieindiza@gmail.com <stephanieindiza@gmail.com>
2025-09-21 13:49:13 +0300

    Task 2 correction

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,11 @@
 const { argv } = require('process');
 
-if (argv.length !== 3) {
+if (argv.length < 3) {
 	console.log("No argument")
-} else {
-	console.log("Argument found")
+}
+if (argv.length == 3) {
+		console.log("Argument found");
+	}
+if (argv.length > 3) {
+		console.log("Arguments found")
 };
